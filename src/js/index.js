78352f639d3f629a5ef02e352e53692d50977d46
Carlos Alberto <caarbelaezg@gmail.com>
2020-05-10 15:53:58 -0500

    modal and banner adjustmens

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,23 @@
+window.onload = () => {
+    window.setTimeout(modalfunc, 2000)
+}
+const modal = document.getElementById('modal')
+const modalfunc = () => {
+    /*Make modal appear*/
+    if (modal.classList.contains('modal-close')) {
+        modal.classList.remove('modal-close')
+        modal.classList.add('modal-open')
+    }
+
+    const closeBtnModal = document.getElementById('modal-close-btn')
+    closeBtnModal.addEventListener('click', closeModal)
+
+}
+
+const closeModal = () => {
+    modal.setAttribute("style", "display:none")
+}
+
 const wpp = document.getElementById('wpp')
 wpp.setAttribute('href', 'https://api.whatsapp.com/send?phone=573008763792&text=Cuéntanos que información quieres saber sobre el AVH')
 
