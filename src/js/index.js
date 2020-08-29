d3b026b22afb386e114fa10754fbd17494ba3fb6
caarbelaezg <caarbelaezg@gmail.com>
2020-08-29 10:38:06 -0500

    Extra content

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,26 @@
-window.onload = () => {
-    window.setTimeout(modalfunc, 2000)
+//window.onload = () => {
+//   if (!sessionStorage.getItem("modal")) {      
+//     window.setTimeout(modalfunc, 2000);
+//     sessionStorage.setItem("modal", "1");
+//   } 
+// };
 
-}
-const modal = document.getElementById('modal')
-const modalfunc = () => {
-    /*Make modal appear*/
-    if (modal.classList.contains('modal-close')) {
-        modal.classList.remove('modal-close')
-        modal.classList.add('modal-open')
-    }
+// const modal = document.getElementById("modal");
 
-    const closeBtnModal = document.getElementById('modal-close-btn')
-    closeBtnModal.addEventListener('click', closeModal)
+// const modalfunc = () => {
+//   /*Make modal appear*/
+//   if (modal.classList.contains("modal-close")) {
+//     modal.classList.remove("modal-close");
+//     modal.classList.add("modal-open");
+//   }
 
-}
+//   const closeBtnModal = document.getElementById("modal-close-btn");
+//   closeBtnModal.addEventListener("click", closeModal);
+// };
 
-const closeModal = () => {
-    modal.setAttribute("style", "display:none")
-}
-
-
-
+// const closeModal = () => {
+//   modal.setAttribute("style", "display:none");
+// };
 
 //Slide principal
 const slideshowImages = document.querySelectorAll(".intro-slideshow img");
@@ -34,11 +34,11 @@ slideshowImages[currentImageCounter].style.opacity = 1;
 setInterval(nextImage, nextImageDelay);
 
 function nextImage() {
-    // slideshowImages[currentImageCounter].style.display = "none";
-    slideshowImages[currentImageCounter].style.opacity = 0;
+  // slideshowImages[currentImageCounter].style.display = "none";
+  slideshowImages[currentImageCounter].style.opacity = 0;
 
-    currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+  currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
 
-    // slideshowImages[currentImageCounter].style.display = "block";
-    slideshowImages[currentImageCounter].style.opacity = 1;
+  // slideshowImages[currentImageCounter].style.display = "block";
+  slideshowImages[currentImageCounter].style.opacity = 1;
 }
