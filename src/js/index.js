6ed529c015a8af3d45f7f5f8c956b75194e34563
caarbelaezg <caarbelaezg@gmail.com>
2020-10-24 17:27:08 -0500

    modal actualization

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,26 @@
-//window.onload = () => {
-//   if (!sessionStorage.getItem("modal")) {      
-//     window.setTimeout(modalfunc, 2000);
-//     sessionStorage.setItem("modal", "1");
-//   } 
-// };
+window.onload = () => {
+  if (!sessionStorage.getItem("modal")) {
+    window.setTimeout(modalfunc, 2000);
+    sessionStorage.setItem("modal", "1");
+  }
+};
 
-// const modal = document.getElementById("modal");
+const modal = document.getElementById("modal");
 
-// const modalfunc = () => {
-//   /*Make modal appear*/
-//   if (modal.classList.contains("modal-close")) {
-//     modal.classList.remove("modal-close");
-//     modal.classList.add("modal-open");
-//   }
+const modalfunc = () => {
+  /*Make modal appear*/
+  if (modal.classList.contains("modal-close")) {
+    modal.classList.remove("modal-close");
+    modal.classList.add("modal-open");
+  }
 
-//   const closeBtnModal = document.getElementById("modal-close-btn");
-//   closeBtnModal.addEventListener("click", closeModal);
-// };
+  const closeBtnModal = document.getElementById("modal-close-btn");
+  closeBtnModal.addEventListener("click", closeModal);
+};
 
-// const closeModal = () => {
-//   modal.setAttribute("style", "display:none");
-// };
+const closeModal = () => {
+  modal.setAttribute("style", "display:none");
+};
 
 //Slide principal
 const slideshowImages = document.querySelectorAll(".intro-slideshow img");
