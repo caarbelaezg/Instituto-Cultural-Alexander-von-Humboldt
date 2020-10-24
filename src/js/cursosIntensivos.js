811aa3cb830c9217c5400e69a43ac70154f3f29c
caarbelaezg <caarbelaezg@gmail.com>
2020-10-24 17:17:41 -0500

    Actualización de cursos

--- src/js/cursosIntensivos.js
+++ src/js/cursosIntensivos.js
@@ -1,41 +1,74 @@
 let nHorarioI = (clase) => {
-    let idName = document.getElementsByClassName(clase)[0].innerHTML
-    let disp = document.getElementsByClassName('actual-disp')[0]
-    console.log(idName)
+  let idName = document.getElementsByClassName(clase)[0].innerHTML;
+  let disp = document.getElementsByClassName("actual-disp")[0];
+  console.log(idName);
 
-    disp.innerHTML = idName === 'A1.1' ? 'A1.1' : idName === 'A1.2' ? 'A1.2' : idName === 'A2.1' ? 'A2.1' : idName === 'A2.2' ? 'A2.2' : idName === 'B1.1' ? 'B1.1' : idName === 'B1.2' ? 'B1.2' : idName === 'B2' ? 'B2' : idName === 'C1' ? 'C1' : idName === 'A1 - Intensivo' ? 'A1 - Intensivo' : 'A1.2 - Semi intensivo'
-    if (idName !== 'A1 - Intensivo' && idName !== 'B2' && idName !== 'C1' && idName !== 'A1.2 - Semi intensivo') {
+  disp.innerHTML =
+    idName === "A1.1"
+      ? "A1.1"
+      : idName === "A1.2"
+      ? "A1.2"
+      : idName === "A2.1"
+      ? "A2.1"
+      : idName === "A2.2"
+      ? "A2.2"
+      : idName === "B1.1"
+      ? "B1.1"
+      : idName === "B1.2"
+      ? "B1.2"
+      : idName === "B1 +"
+      ? "B1 +"
+      : idName === "B2"
+      ? "B2"
+      : idName === "C1"
+      ? "C1"
+      : idName === "A1 - Super Intensivo"
+      ? "A1 - Super Intensivo"
+      : "A2 - Super Intensivo";
 
-        //Tratamiento especial días
-        document.getElementsByClassName('day-1')[0].innerHTML = 'Lunes a viernes'
-            //Tratamiento especial horas
-        document.getElementsByClassName('hor-1')[0].innerHTML = '6:00pm - 8:30pm'
-            //Tratamiento especial sedes
-        document.getElementsByClassName('sed-1')[0].innerHTML = 'Calazans'
-
-    } else if (idName === 'A1 - Intensivo') {
-        //Tratamiento especial días
-        document.getElementsByClassName('day-1')[0].innerHTML = 'Lunes a viernes'
-            //Tratamiento especial horas
-        document.getElementsByClassName('hor-1')[0].innerHTML = '8:00am - 1:00pm'
-            //Tratamiento especial sedes
-        document.getElementsByClassName('sed-1')[0].innerHTML = 'Calazans'
-
-    } else if (idName === 'A1.2 - Semi intensivo') {
-        //Tratamiento especial días
-        document.getElementsByClassName('day-1')[0].innerHTML = 'Lunes, miercoles y viernes'
-            //Tratamiento especial horas
-        document.getElementsByClassName('hor-1')[0].innerHTML = '6:00pm - 8:30pm'
-            //Tratamiento especial sedes
-        document.getElementsByClassName('sed-1')[0].innerHTML = 'Calazans - Este curso no finaliza en la misma fecha de los intensivos.'
-
-    } else {
-        //Tratamiento especial días
-        document.getElementsByClassName('day-1')[0].innerHTML = 'Se ofertará nuevamente en Agosto'
-            //Tratamiento especial horas
-        document.getElementsByClassName('hor-1')[0].innerHTML = ''
-            //Tratamiento especial sedes
-        document.getElementsByClassName('sed-1')[0].innerHTML = ''
-
-    }
-}
+  if (idName === "A1.1" || idName === "A1.2") {
+    //Tratamiento especial días
+    document.getElementsByClassName("day-1")[0].innerHTML = "Lunes a viernes";
+    //Tratamiento especial horas
+    document.getElementsByClassName("hor-1")[0].innerHTML = "9:00am - 11:30am";
+    document.getElementsByClassName("hor-2")[0].innerHTML = "6:00pm - 8:30pm";
+    document.getElementsByClassName("hor-2")[0].style.display = "block";
+    //Tratamiento especial sedes
+    document.getElementsByClassName("sed-1")[0].innerHTML = "Virtual";
+  } else if (
+    idName === "A1 - Super Intensivo" ||
+    idName === "A2 - Super Intensivo"
+  ) {
+    //Tratamiento especial días
+    document.getElementsByClassName("day-1")[0].innerHTML = "Lunes a sábado";
+    //Tratamiento especial horas
+    document.getElementsByClassName("hor-1")[0].innerHTML =
+      "Lunes a viernes 5:00pm - 9:00pm <br> Sábados 8:00am - 12:30pm";
+    document.getElementsByClassName("hor-2")[0].style.display = "none";
+    //Tratamiento especial sedes
+    document.getElementsByClassName("sed-1")[0].innerHTML = "Virtual";
+  } else if (
+    idName === "A2.1" ||
+    idName === "A2.2" ||
+    idName === "B1.1" ||
+    idName === "B1.2" ||
+    idName === "B1 +"
+  ) {
+    //Tratamiento especial días
+    document.getElementsByClassName("day-1")[0].innerHTML = "Lunes a viernes";
+    //Tratamiento especial horas
+    document.getElementsByClassName("hor-1")[0].innerHTML = "6:00pm - 8:30pm";
+    document.getElementsByClassName("hor-2")[0].style.display = "none";
+    //Tratamiento especial sedes
+    document.getElementsByClassName("sed-1")[0].innerHTML = "Virtual";
+  } else {
+    //Tratamiento especial días
+    document.getElementsByClassName("day-1")[0].innerHTML =
+      "Se ofertará nuevamente en Febrero";
+    //Tratamiento especial horas
+    document.getElementsByClassName("hor-1")[0].innerHTML = "";
+    document.getElementsByClassName("hor-2")[0].style.display = "none";
+    //Tratamiento especial sedes
+    document.getElementsByClassName("sed-1")[0].innerHTML = "";
+  }
+};
